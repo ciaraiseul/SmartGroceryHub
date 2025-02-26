11759ef67096a19ae35137cb5d6d6c9c05a40f29
ciaraiseul <139107560+ciaraiseul@users.noreply.github.com>
2025-02-27 00:07:26 +0700

    update

--- SmartGroceryHub/View/MainTab/MainTabView.swift
+++ SmartGroceryHub/View/MainTab/MainTabView.swift
@@ -31,61 +31,7 @@ struct MainTabView: View {
             
             HStack{
                 
-                TabButton(title: "Trang chủ",
-                          icon: "store_tab",
-                          isSelect: homeVM.selectTab == 0)
-                {
-                    print("Button tab")
-                    DispatchQueue.main.async {
-                        withAnimation {
-                            homeVM.selectTab = 0
-                        }
-                    }
-                }
                 
-                TabButton(title: "Khám phá",
-                          icon: "explore_tab",
-                          isSelect: homeVM.selectTab == 1)
-                {
-                    DispatchQueue.main.async {
-                        withAnimation {
-                            homeVM.selectTab = 1
-                        }
-                    }
-                }
-                
-                TabButton(title: "Giỏ hàng",
-                          icon: "cart_tab",
-                          isSelect: homeVM.selectTab == 2)
-                {
-                    DispatchQueue.main.async {
-                        withAnimation {
-                            homeVM.selectTab = 2
-                        }
-                    }
-                }
-                
-                TabButton(title: "Yêu thích",
-                          icon: "fav_tab",
-                          isSelect: homeVM.selectTab == 3)
-                {
-                    DispatchQueue.main.async {
-                        withAnimation {
-                            homeVM.selectTab = 3
-                        }
-                    }
-                }
-                
-                TabButton(title: "Tài khoản",
-                          icon: "account_tab",
-                          isSelect: homeVM.selectTab == 4)
-                {
-                    DispatchQueue.main.async {
-                        withAnimation {
-                            homeVM.selectTab = 4
-                        }
-                    }
-                }
                 
                 
             }
